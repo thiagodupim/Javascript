866a8006062cf3a4eda8f7db3c60fbfd79308ac8
thiagodupim <thiagodupim3@gmail.com>
2022-04-08 11:06:50 -0300

    Initial commit

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+while(c <= 3){
+    console.log('Repetindo...')
+    c++
+}
+
+var c = 1
+do{
+    console.log('Repetindo...')
+    c++
+} while(c <= 3)
+
+for(var c = 1; c <= 3; c++){
+    console.log('Repetindo...')
+}
